7c8046af9291f5e37f1bd31183a4594be40eff72
OrfeasANikolaou <orfeasnikolaou.dev@gmail.com>
2023-12-04 21:04:21 +0200

    added better data types header for easier personal use

---
+++ headers/better_datatypes.h
@@ -0,0 +1,13 @@
+#include <stdint.h>
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8; // signed char NEVER USE THIS
+typedef uint8_t u8; // unsigned char
+typedef float f32;
+typedef double f64;
+typedef long double f128;
+typedef wchar_t fat_char;
